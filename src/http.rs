743686c73552323bc50fd761d5cf7957c1ec04a8
Kennedy Oliveira <kennedyroliveira@gmail.com>
2024-10-18 12:04:33 -0300

    feat(http): Add ETag support for query endpoints

--- src/http.rs
+++ src/http.rs
@@ -1,26 +1,48 @@
 use axum::http::header;
 use axum::response::{IntoResponse, Response};
 use axum::Json;
+use axum_extra::headers::{ContentType, ETag, HeaderMapExt};
 use bytes::Bytes;
 use reqwest::StatusCode;
 use serde_json::json;
 
 const APPLICATION_JSON_MEDIA_TYPE: &str = "application/json";
 
-/// Content of proxied response from HowLongToBeat
-#[derive(Debug, Clone)]
-pub(crate) struct HtlbProxiedResponse {
-    /// Content of the response from HLTB
-    pub content: Bytes,
+pub(crate) enum CachedResponse<T>
+where
+    T: IntoResponse,
+{
+    Fresh(T),
+    NotModified,
 }
 
-impl IntoResponse for HtlbProxiedResponse {
+impl<T> IntoResponse for CachedResponse<T>
+where
+    T: IntoResponse,
+{
     fn into_response(self) -> Response {
-        (
-            [(header::CONTENT_TYPE, APPLICATION_JSON_MEDIA_TYPE)],
-            self.content,
-        )
-            .into_response()
+        match self {
+            CachedResponse::Fresh(response) => response.into_response(),
+            CachedResponse::NotModified => (StatusCode::NOT_MODIFIED, "").into_response(),
+        }
+    }
+}
+
+/// Content of proxied response from HowLongToBeat
+#[derive(Debug, Clone)]
+pub(crate) struct HltbProxiedResponse {
+    /// Content of the response from HLTB
+    pub content: Bytes,
+    pub etag: ETag,
+}
+
+impl IntoResponse for HltbProxiedResponse {
+    fn into_response(self) -> Response {
+        let mut headers = header::HeaderMap::new();
+        headers.typed_insert(self.etag);
+        headers.typed_insert(ContentType::json());
+
+        (headers, self.content).into_response()
     }
 }
 
